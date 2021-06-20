3b0a4c5dc0eca13ca150c77dc2e771f2bacf1462
Htet-Yadana <84025200+Htet-Yadana@users.noreply.github.com>
2021-06-20 14:38:36 +0630

    Add files via upload

---
+++ lib/Profile/banner.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class BannerWidget extends StatelessWidget {
+  final String imgUrl;
+  final String title;
+  BannerWidget({
+    required this.imgUrl,
+    required this.title,
+  })
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 350,
+      width: double.infinity,
+      color: Colors.black26,
+      child: (Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage(imgUrl),
+            width: 100,
+            height: 150,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      )),
+    );
+  }
+}
